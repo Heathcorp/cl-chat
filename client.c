e342c125ad20d63a690637ec505877db2a791fb8
Heathcorp <heath@manningf.com>
2022-02-12 17:32:01 +1100

    Started learning socket programming

--- client.c
+++ client.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	int socket_desc;
+	if ((socket_desc = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		puts("Failed to create socket");
+		return 1;
+	}
+
+	struct sockaddr_in other;
+	other.sin_addr.s_addr = inet_addr("127.0.0.1");
+	other.sin_family = AF_INET;
+	other.sin_port = htons(8888);
+
+	if (connect(socket_desc, (struct sockaddr *)&other, sizeof(other)) < 0)
+	{
+		puts("Failed to connect");
+		return 1;
+	}
+
+	char *message = "client 1 wants to chat";
+	if (send(socket_desc, message, 22, 0) < 0)
+	{
+		puts("Failed to send");
+		return 1;
+	}
+
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
+	int socket_desc;
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in other;
+	other.sin_addr.s_addr = inet_addr("127.0.0.1");
+	other.sin_family = AF_INET;
+	other.sin_port = htons(8888);
+
+	connect(socket_desc, (struct sockaddr *)&other, sizeof(other));
+
+	char *message = "                      ";
+	recv(socket_desc, message, 22, 0);
+
+	puts(message);
+
 	return 0;
 }
