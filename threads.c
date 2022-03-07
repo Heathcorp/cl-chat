fbc22a38f348141eb64152b86b5de1948927decf
Heathcorp <heath@manningf.com>
2022-03-07 22:27:48 +1100

    Send usernames before initiating conversation

--- threads.c
+++ threads.c
@@ -1,6 +1,3 @@
-#include <sys/socket.h>
-#include <arpa/inet.h>
-
 struct thread_config {
     int sockfd;
     struct user_data* user_other;
@@ -18,34 +15,28 @@ void* recv_routine(void* config) {
     logmsg(sys, "Receive thread started");
 
     struct chat_message msg;
+    char msgType;
 
-    // // first get the other user's name
-    // size_t n;
-    // recv(sockfd, &n, sizeof(n), 0);
-    // printf("%d\n", n);
-    // other->username = malloc(n + 1);
-    // printf("%d\n", n);
-    // recv(sockfd, other->username, n, 0);
-    // other->username[n] = '\0';
-    // printf("%d\n", n);
-    // printf("%s\n", other->username);
+    while (recv(sockfd, &msgType, sizeof(msgType), 0) > 0) {
+        recvcontent(sockfd, &msg);
 
-    while (recv(sockfd, &msg.size, sizeof(msg.size), 0) > 0) {
-        // printf("Receiving %d bytes\n", msg.size);
+        switch (msgType) {
+            case M_NAME:
+                other->username = msg.content;
+                // avoiding freeing the memory here as I want it to persist
+                msg.content = NULL;
+                break;
+            case M_MESSAGE:
+                logmsg(other, msg.content);
+                break;
+        }
 
-        // allocate enough for the incoming message + null terminator
-        msg.content = malloc(msg.size + 1);
-        recv(sockfd, msg.content, msg.size, 0);
-        // add the null terminator
-        msg.content[msg.size] = '\0';
-
-        logmsg(other, msg.content);
 
         free(msg.content);
         msg.content = NULL;
         msg.size = 0;
     }
-
+    
     free(other->username);
 }
 
@@ -58,12 +49,16 @@ void* send_routine(void* config) {
 
     logmsg(sys, "Send thread started");
 
-    // // first send my username
-    // size_t n = strlen(me->username);
-    // send(sockfd, &n, sizeof(n), 0);
-    // send(sockfd, me->username, n, 0);
-
     struct chat_message msg;
+
+    // send my username
+    msg.content = me->username;
+    msg.size = strlen(me->username);
+    sendmessage(sockfd, M_NAME, &msg);
+
+    msg.content = NULL;
+    msg.size = 0;
+
     size_t n = 0;
     while ((msg.size = getline(&msg.content, &n, stdin)) != -1) {
         // printf("Sending %d bytes\n", msg.size);
@@ -71,10 +66,7 @@ void* send_routine(void* config) {
         // strip off newline the dodgy way:
         msg.size--;
 
-        // send size of message
-        send(sockfd, &msg.size, sizeof(msg.size), 0);
-        // send message content
-        send(sockfd, msg.content, msg.size, 0);
+        sendmessage(sockfd, M_MESSAGE, &msg);
 
         free(msg.content);
         msg.content = NULL;
