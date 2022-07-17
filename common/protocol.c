79464c80be4c9da6cc3ec6e8c207042e31ed031e
Heathcorp <heath@manningf.com>
2022-07-17 15:50:11 +1000

    Implement registration and messaging commands

--- common/protocol.c
+++ common/protocol.c
@@ -28,12 +28,48 @@ int protocol_command(int sockfd, char code, void* contents, size_t bufsize) {
 	buf[n - 1] = '\n';
 
 	// send the message
-	send(sockfd, buf, n, NULL);
+	send(sockfd, buf, n, 0);
+	
+	free(buf);
 }
 
 int send_disconnect(int sockfd, char reason) {
+	size_t n = 1;
 	char* buf = " ";
 	buf[0] = reason;
 
-	return protocol_command(sockfd, 'Q', buf, 1);
+	return protocol_command(sockfd, 'Q', buf, n);
+}
+
+int send_register(int sockfd, char* username, size_t bufsize) {
+	size_t n = bufsize + 2;
+	char* buf = malloc(n);
+
+	// write the username length
+	buf[0] = (char)bufsize;
+	buf[1] = '\n';
+	// add the username
+	memcpy(buf + 2, username, bufsize);
+
+	int ret = protocol_command(sockfd, 'R', buf, n);
+	free(buf);
+	return ret;
+}
+
+int send_message(int sockfd, char* targetusr, size_t usrsize, char* message, size_t msgsize) {
+	size_t n = usrsize + 3 + msgsize;
+	char* buf = malloc(n);
+
+	// copy username
+	memcpy(buf, targetusr, usrsize);
+	buf[usrsize] = '\n';
+	// write message size
+	buf[usrsize + 1] = (char)msgsize;
+	buf[usrsize + 2] = '\n';
+	// copy the message
+	memcpy(buf + usrsize + 3, message, msgsize);
+
+	int ret = protocol_command(sockfd, 'M', buf, n);
+	free(buf);
+	return ret;
 }
