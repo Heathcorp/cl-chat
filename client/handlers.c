170e09275782f77aeef3263e02a9592ddc3f9238
Heathcorp <heath@manningf.com>
2022-08-28 23:33:47 +1000

    Begin implementing clientside message sending loop

--- client/handlers.c
+++ client/handlers.c
@@ -11,10 +11,19 @@ void* send_routine(void* conf) {
 	int sockfd = ((struct thread_config*)conf)->sockfd;
 	// printf("Started send thread: sockfd = %d\n", sockfd);
 
+	printf("Please type your username:\n");
 	char* buffer = NULL;
 	size_t n = 0;
 	size_t usrsize = getline(&buffer, &n, stdin);
 	send_register(sockfd, buffer, usrsize - 1);
 	free(buffer);
-	// this may not work because the socket will close immediately and cause the server to read 0 bytes
+
+	for(;;) {
+		buffer = NULL;
+		size_t msg_len = getline(&buffer, &n, stdin);
+
+		send_register(sockfd, buffer, msg_len - 1);
+
+		free(buffer);
+	}
 }
